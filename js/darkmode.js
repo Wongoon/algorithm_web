a43619172e4992fc453193693631119a1e788d91
Wongoon <rmsgh64@gmail.com>
2023-04-02 18:58:46 +0900

    code update

--- js/darkmode.js
+++
@@ -1,7 +0,0 @@
-const body = document.querySelector('body');
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', () => {
-    btn.classList.toggle('active');
-    body.classList.toggle('dark');
-});
--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
-jQuery(document).ready(function(){
-    $('.nav > li').mouseover(function(){
-        $(this).find('.nav_in').stop().slideDown(500);
-    }).mouseout(function(){
-        $(this).find('.nav_in').stop().slideUp(500);
-    });
+const body = document.querySelector('body');
+const btn = document.getElementById('btn');
+const dontmove = document.querySelector('.dontmove');
+const menu = document.querySelector('#sub');
+const navIn = document.querySelector('.nav_in');
+
+let toggle = true;
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('active');
+    body.classList.toggle('dark');
+});
+
+dontmove.addEventListener('click', (e) => {
+    e.preventDefault();
+});
+
+menu.addEventListener("click", function(){
+    if(toggle){
+        navIn.style.display = 'block';
+        navIn.classList.add('down');
+        navIn.classList.remove('up');
+
+        toggle = !toggle;
+    }
+    else{
+        navIn.classList.add('up');
+        navIn.classList.remove('down');
+        
+        toggle = !toggle;
+    }
 });
